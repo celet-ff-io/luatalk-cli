e5f61520616fac182dd575363f3d82eab0fe65a3
celet-ff-io <connectingstarts@outlook.com>
2026-06-24 20:18:09 +0800

    Use flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "luatalk-cli";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    {
+      nixpkgs,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          musl
+        ];
+      };
+    };
+
+}
