f123ed115a7178fc8ce9d899c3c579af599df78e
celet-ff-io <connectingstarts@outlook.com>
2026-06-27 07:18:56 +0800

    Add lib-default to load talk.lua

--- src/bin/luatalk/app.rs
+++ src/bin/luatalk/app.rs
@@ -9,9 +9,13 @@ use clap::{
 };
 use clap_stdin::{FileOrStdin, FileOrStdout};
 use clap_verbosity_flag::{InfoLevel, Verbosity};
+use const_format::formatcp;
 use log::debug;
 use miette::{IntoDiagnostic, Result, WrapErr};
-use mlua::Lua;
+use mlua::{
+    Lua,
+    prelude::{LuaTable, LuaValue},
+};
 use tap::Pipe;
 
 use luatalk::{Article, lua};
@@ -30,6 +34,13 @@ pub(crate) struct App {
     /// Input Lua file. '-' for stdin.
     input: FileOrStdin,
 
+    // #[arg(long = "lib", action = ArgAction::Append)]
+    // libs: Vec<PathBuf>,
+
+    // Load the module `talk.lua`.
+    #[arg(long)]
+    lib_default: bool,
+
     /// Ouptut file. '-' for stdout. Defaults to stdout.
     #[arg(short, long, default_value = "-")]
     output: FileOrStdout,
@@ -41,6 +52,7 @@ pub(crate) struct App {
 impl App {
     pub(crate) fn run(self) -> Result<()> {
         let source;
+        // let lib: Vec<PathBuf>;
         let mut writer;
         {
             let input = self.input;
@@ -57,6 +69,27 @@ impl App {
         };
 
         let lua = Lua::new();
+
+        if self.lib_default {
+            debug!("Loading default libs");
+
+            let loaded_modules: LuaTable = lua.load("package.loaded").eval().into_diagnostic()?;
+
+            const LUA_MODULE_NAME: &str = "talk";
+            let lib_module: LuaValue = lua
+                .load(include_str!("../../../assets/lua/lib/talk.lua"))
+                .set_name(formatcp!("{LUA_MODULE_NAME}.lua"))
+                .eval()
+                .into_diagnostic()
+                .wrap_err("Failed to load `talk.lua` content")?;
+
+            debug!("Loaded module `{LUA_MODULE_NAME}`: {lib_module:?}");
+
+            loaded_modules
+                .set(LUA_MODULE_NAME, lib_module)
+                .into_diagnostic()
+                .wrap_err("Failed to load `talk.lua` as module")?;
+        }
 
         let article = lua::Article::from_chunk(&source, &lua)
             .into_diagnostic()?
