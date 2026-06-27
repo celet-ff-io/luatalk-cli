54f90f3cc6e2ab8476c5b7b50b5225d4d5add1ca
celet-ff-io <connectingstarts@outlook.com>
2026-06-27 14:04:01 +0800

    Add --lib as alternative way to enable features of talk.lua

--- src/bin/luatalk/app.rs
+++ src/bin/luatalk/app.rs
@@ -1,7 +1,10 @@
-use std::io::{BufWriter, Write};
+use std::{
+    io::{BufWriter, Write},
+    path::{Path, PathBuf},
+};
 
 use clap::{
-    Parser,
+    ArgAction, Parser,
     builder::{
         Styles,
         styling::{AnsiColor, Effects},
@@ -9,10 +12,10 @@ use clap::{
 };
 use clap_stdin::{FileOrStdin, FileOrStdout};
 use clap_verbosity_flag::{InfoLevel, Verbosity};
-use log::debug;
+use log::{debug, warn};
 use miette::{IntoDiagnostic, Result, WrapErr};
-use mlua::Lua;
-use tap::Pipe;
+use mlua::{Lua, Table};
+use tap::{Pipe, Tap};
 
 use luatalk::{Article, LuaTalkExt, lua};
 
@@ -30,12 +33,13 @@ pub struct Args {
     /// Input Lua file. '-' for stdin.
     input: FileOrStdin,
 
-    // #[arg(long = "lib", action = ArgAction::Append)]
-    // libs: Vec<PathBuf>,
-
     // Load the module `talk.lua` hard-coded in program.
     #[arg(long)]
     lib_default: bool,
+
+    /// Additional search directories for Lua modules. Can be specified multiple times.
+    #[arg(long = "lib", action = ArgAction::Append)]
+    libs: Vec<PathBuf>,
 
     /// Ouptut file. '-' for stdout. Defaults to stdout.
     #[arg(short, long, default_value = "-")]
@@ -47,7 +51,11 @@ pub struct Args {
 
 pub struct App {
     source: String,
+
     lib_default: bool,
+
+    path_addtion: String,
+
     writer: BufWriter<Box<dyn Write>>,
 
     lua: Lua,
@@ -55,20 +63,48 @@ pub struct App {
 
 impl App {
     pub fn new(args: Args) -> Result<Self> {
+        env_logger::Builder::new()
+            .filter_level(args.verbose.log_level_filter())
+            .init();
+
         let source;
-        // let lib: Vec<PathBuf>;
         let lib_default = args.lib_default;
+        let path_addtion: String;
         let writer;
+
         {
             let input = args.input;
             debug!("Input file: {}", input.filename());
-
             source = input
                 .contents()
                 .into_diagnostic()
                 .wrap_err("Input file not found")?;
-            let output = args.output;
 
+            const INITIAL_CAPACITY: usize = 128;
+            path_addtion = args
+                .libs
+                .iter()
+                .filter(|p| {
+                    p.is_dir().tap(|&is| {
+                        if is {
+                            debug!("Add directory path: {}", p.display());
+                        } else {
+                            warn!("Ignore bad directory path: {}", p.display());
+                        }
+                    })
+                })
+                .flat_map(|p| {
+                    [
+                        Self::as_lua_path_entry_or_empty(p, "?.lua"),
+                        Self::as_lua_path_entry_or_empty(p, "?/init.lua"),
+                    ]
+                })
+                .fold(String::with_capacity(INITIAL_CAPACITY), |mut acc, s| {
+                    acc.push_str(&s);
+                    acc
+                });
+
+            let output = args.output;
             debug!("Output file: {}", output.filename());
             writer = output
                 .into_writer()
@@ -81,10 +117,17 @@ impl App {
         Ok(Self {
             source,
             lib_default,
+            path_addtion,
             writer,
 
             lua: Lua::new(),
         })
+    }
+
+    fn as_lua_path_entry_or_empty(dir: &Path, file: &str) -> String {
+        dir.join(file)
+            .to_str()
+            .map_or(String::new(), |s| s.to_owned() + ";")
     }
 
     pub fn run(self) -> Result<()> {
@@ -93,6 +136,15 @@ impl App {
         if self.lib_default {
             debug!("Loading default lib");
             lua.load_default_lib().into_diagnostic()?;
+        }
+
+        if !self.path_addtion.is_empty() {
+            const KEY_PATH: &str = "path";
+            let pacakges: Table = lua.globals().get("package").into_diagnostic()?;
+            let current_path: String = pacakges.get(KEY_PATH).into_diagnostic()?;
+            let new_path = self.path_addtion + &current_path;
+            debug!("Lua package path will update to {new_path}");
+            pacakges.set(KEY_PATH, new_path).into_diagnostic()?;
         }
 
         let article = lua::Article::from_chunk(&self.source, &lua)
